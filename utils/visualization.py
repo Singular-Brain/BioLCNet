f05c1bdf7e38af55f9911ebee072372b6bfe9cca
Hafez Ghaemi <ghaemi.hafez@gmail.com>
2021-11-04 22:16:37 +0100

    Organize repo

--- utils/visualization.py
+++ utils/visualization.py
@@ -18,18 +18,16 @@ from torch.nn.modules.utils import _pair
 
 
 
-
-
-def reshape_locally_connected_weights(
+def reshape_LC_weights(
     w: torch.Tensor,
     n_filters: int,
     kernel_size: Union[int, Tuple[int, int]],
     conv_size: Union[int, Tuple[int, int]],
     input_sqrt: Union[int, Tuple[int, int]],
 ) -> torch.Tensor:
-
+    # language=rst
     """
-    Get the weights from a locally connected layer slice (channel) and reshape them to be two-dimensional and square.
+    Get the weights from a locally connected layer and reshape them to be two-dimensional and square.
     :param w: Weights from a locally connected layer.
     :param n_filters: No. of neuron filters.
     :param kernel_size: Side length(s) of convolutional kernel.
@@ -37,14 +35,10 @@ def reshape_locally_connected_weights(
     :param input_sqrt: Sides length(s) of input neurons.
     :return: Locally connected weights reshaped as a collection of spatially ordered square grids.
     """
-
-    kernel_size = _pair(kernel_size)
-    conv_size = _pair(conv_size)
-    input_sqrt = _pair(input_sqrt)
-
     k1, k2 = kernel_size
     c1, c2 = conv_size
     i1, i2 = input_sqrt
+    c1sqrt, c2sqrt = int(math.ceil(math.sqrt(c1))), int(math.ceil(math.sqrt(c2)))
     fs = int(math.ceil(math.sqrt(n_filters)))
 
     w_ = torch.zeros((n_filters * k1, k2 * c1 * c2))
@@ -84,7 +78,6 @@ def reshape_locally_connected_weights(
                             ]
 
         return square
-
 
 
 
@@ -178,254 +171,196 @@ def plot_fully_connected_weights(
         return im
 
 
+def plot_LC_weights(lc : object,
+    input_channel: int = 0,
+    output_channel: int = None,
+    lines: bool = True,
+    figsize: Tuple[int, int] = (5, 5),
+    cmap: str = "hot_r",
+    color: str='r',
+    ) -> AxesImage:
+    # language=rst
+    """
+    Plot a connection weight matrix of a :code:`Connection` with `locally connected
+    structure <http://yann.lecun.com/exdb/publis/pdf/gregor-nips-11.pdf>_.
+    :param lc: LC connection object of LCNet
+    :param input_channel: indicates weights which connected to this channel of input 
+    :param output_channel: indicates weights of specific channel in the output layer
+    :param lines: Whether or not to draw horizontal and vertical lines separating input regions.
+    :param figsize: Horizontal, vertical figure size in inches.
+    :param cmap: Matplotlib colormap.
+    :return: Used for re-drawing the weights plot.
+    """
 
-def plot_locally_connected_feature_maps(weights: torch.Tensor,
-    n_filters: int,
-    in_chans: int,
-    slice_to_plot: int,
-    input_size: int,
-    kernel_size: Union[int, Tuple[int, int]],
-    conv_size: Union[int, Tuple[int, int]],
-    wmin: float = 0.0,
-    wmax: float = 1.0,
-    im: Optional[AxesImage] = None,
+    n_sqrt = int(np.ceil(np.sqrt(lc.out_channels)))
+    sel_slice = lc.w.view(lc.in_channels, lc.out_channels, lc.conv_size[0], lc.conv_size[1], lc.kernel_size[0], lc.kernel_size[1]).cpu()
+    input_size = _pair(int(np.sqrt(lc.source.n)))
+    
+    if output_channel is None:
+        sel_slice = sel_slice[input_channel, ...]
+        reshaped = reshape_LC_weights(sel_slice, lc.out_channels, lc.kernel_size, lc.conv_size, input_size)
+    else:
+        sel_slice = sel_slice[input_channel, output_channel, ...]
+        sel_slice = sel_slice.unsqueeze(0)
+        reshaped = reshape_LC_weights(sel_slice, 1, lc.kernel_size, lc.conv_size, input_size)
+        print(reshaped.shape)
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    im = ax.imshow(reshaped.cpu(), cmap=cmap, vmin=lc.wmin, vmax=lc.wmax)
+    div = make_axes_locatable(ax)
+    cax = div.append_axes("right", size="5%", pad=0.05)
+
+    if lines and  output_channel is None:
+        for i in range(
+            n_sqrt * lc.kernel_size[0],
+            n_sqrt * lc.conv_size[0] * lc.kernel_size[0],
+            n_sqrt * lc.kernel_size[0],
+        ):
+            ax.axhline(i - 0.5, color=color, linestyle="--")
+
+        for i in range(
+            n_sqrt * lc.kernel_size[1],
+            n_sqrt * lc.conv_size[1] * lc.kernel_size[1],
+            n_sqrt * lc.kernel_size[1],
+        ):
+            ax.axvline(i - 0.5, color=color, linestyle="--")
+
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_aspect("auto")
+
+    plt.colorbar(im, cax=cax)
+    fig.tight_layout()
+
+    return im
+    
+
+def plot_LC_activation_map(lc : object,
+    spikes: torch.tensor,
+    input_channel: int = 0,
+    scale_factor: float = 1.0,
     lines: bool = True,
     figsize: Tuple[int, int] = (5, 5),
     cmap: str = "hot_r",
     color: str='r'
     ) -> AxesImage:
-
+    # language=rst
     """
-    Plot a 2D local connection slice feature map
-    :param weights: Weight matrix of Conv2dConnection object.
-    :param n_filters: No. of convolution kernels in use.
-    :param in_channels: No. of input channels
-    :param slice_to_plot: The 2D slice to plot
-    :param input_size: The square input size
-    :param kernel_size: Side length(s) of 2D convolution kernels.
-    :param conv_size: Side length(s) of 2D convolution population.
-    :param wmin: Minimum allowed weight value.
-    :param wmax: Maximum allowed weight value.
-    :param im: Used for re-drawing the weights plot.
-    :param lines: Whether or not to draw horizontal and vertical lines separating input
-        regions.
+    Plot an activation map of a :code:`Connection` with `locally connected
+    structure <http://yann.lecun.com/exdb/publis/pdf/gregor-nips-11.pdf>_.
+    :param lc: LC connection object of LCNet
+    :param input_channel: indicates weights which connected to this channel of input 
+    :param scale_factor: determines intensity of activation map 
+    :param lines: Whether or not to draw horizontal and vertical lines separating input regions.
     :param figsize: Horizontal, vertical figure size in inches.
     :param cmap: Matplotlib colormap.
     :return: Used for re-drawing the weights plot.
     """
-
-    n_sqrt = int(np.ceil(np.sqrt(n_filters)))
-
-    sel_slice = weights.view(in_chans, n_filters, conv_size, conv_size, kernel_size, kernel_size).cpu()
-    sel_slice = sel_slice[slice_to_plot, ...]
-    
-    reshaped = reshape_locally_connected_weights(
-        sel_slice, n_filters, kernel_size, conv_size, input_size
-    )
-
-    if not im:
-        fig, ax = plt.subplots(figsize=figsize)
-
-        im = ax.imshow(reshaped.cpu(), cmap=cmap, vmin=wmin, vmax=wmax)
-        div = make_axes_locatable(ax)
-        cax = div.append_axes("right", size="5%", pad=0.05)
-        kernel_size = _pair(kernel_size)
-        conv_size = _pair(conv_size)
-        
-        if lines:
-            for i in range(
-                n_sqrt * kernel_size[0],
-                n_sqrt * conv_size[0] * kernel_size[0],
-                n_sqrt * kernel_size[0],
-            ):
-                ax.axhline(i - 0.5, color=color, linestyle="--")
-
-            for i in range(
-                n_sqrt * kernel_size[1],
-                n_sqrt * conv_size[1] * kernel_size[1],
-                n_sqrt * kernel_size[1],
-            ):
-                ax.axvline(i - 0.5, color=color, linestyle="--")
-
-        ax.set_xticks(())
-        ax.set_yticks(())
-        ax.set_aspect("auto")
-
-        plt.colorbar(im, cax=cax)
-        fig.tight_layout()
-    else:
-        im.set_data(reshaped)
-
-    return im
-    
-def plot_locally_connected_activations(
-    weights: torch.Tensor,
-    spikes : torch.Tensor,
-    n_filters: int,
-    in_chans: int,
-    slice_to_plot: int,
-    input_size: int,
-    kernel_size: Union[int, Tuple[int, int]],
-    conv_size: Union[int, Tuple[int, int]],
-    wmin: float = 0.0,
-    wmax: float = 1.0,
-    scale_factor : float = 1.0,
-    im: Optional[AxesImage] = None,
-    lines: bool = True,
-    figsize: Tuple[int, int] = (5, 5),
-    cmap: str = "hot_r",
-    color: str='r'
-) -> AxesImage:
-    """
-    Plot the activations of a 2D locally connected slice
-    :param weights: Weight matrix of Conv2dConnection object.
-    :param n_filters: No. of convolution kernels in use.
-    :param in_channels: No. of input channels
-    :param slice_to_plot: The 2D slice to plot
-    :param input_size: The square input size
-    :param kernel_size: Side length(s) of 2D convolution kernels.
-    :param conv_size: Side length(s) of 2D convolution population.
-    :param wmin: Minimum allowed weight value.
-    :param wmax: Maximum allowed weight value.
-    :param im: Used for re-drawing the weights plot.
-    :param lines: Whether or not to draw horizontal and vertical lines separating input
-        regions.
-    :param figsize: Horizontal, vertical figure size in inches.
-    :param cmap: Matplotlib colormap.
-    :return: Used for re-drawing the weights plot.
-    """
+    spikes = spikes.sum(0).squeeze().view(lc.conv_size[0]*int(np.sqrt(lc.out_channels)),lc.conv_size[1]*int(np.sqrt(lc.out_channels)))
     x = scale_factor * spikes / torch.max(spikes)
-    x = x.clip(wmin,wmax).repeat_interleave(kernel_size, dim=0).repeat_interleave(kernel_size, dim=1).cpu()
-    n_sqrt = int(np.ceil(np.sqrt(n_filters)))
+    x = x.clip(lc.wmin,lc.wmax).repeat_interleave(lc.kernel_size[0], dim=0).repeat_interleave(lc.kernel_size[1], dim=1).cpu()
+    n_sqrt = int(np.ceil(np.sqrt(lc.out_channels)))
 
-    sel_slice = weights.view(in_chans, n_filters, conv_size, conv_size, kernel_size, kernel_size).cpu()
-    sel_slice = sel_slice[slice_to_plot, ...]
+    sel_slice = lc.w.view(lc.in_channels, lc.out_channels, lc.conv_size[0], lc.conv_size[1], lc.kernel_size[0], lc.kernel_size[1]).cpu()
+    sel_slice = sel_slice[input_channel, ...]
+    input_size = _pair(int(np.sqrt(lc.source.n)))
+    reshaped = reshape_LC_weights(sel_slice, lc.out_channels, lc.kernel_size, lc.conv_size, input_size)
 
-    reshaped = reshape_locally_connected_weights(
-        sel_slice, n_filters, kernel_size, conv_size, input_size
-    )
+    fig, ax = plt.subplots(figsize=figsize)
 
-    if not im:
-        fig, ax = plt.subplots(figsize=figsize)
+    im = ax.imshow(reshaped.cpu()*x, cmap=cmap, vmin=lc.wmin, vmax=lc.wmax)
+    div = make_axes_locatable(ax)
+    cax = div.append_axes("right", size="5%", pad=0.05)
 
-        im = ax.imshow(reshaped.cpu()*x, cmap=cmap, vmin=wmin, vmax=wmax)
-        div = make_axes_locatable(ax)
-        cax = div.append_axes("right", size="5%", pad=0.05)
-        kernel_size = _pair(kernel_size)
-        conv_size = _pair(conv_size)
-        
-        if lines:
-            for i in range(
-                n_sqrt * kernel_size[0],
-                n_sqrt * conv_size[0] * kernel_size[0],
-                n_sqrt * kernel_size[0],
-            ):
-                ax.axhline(i - 0.5, color=color, linestyle="--")
+    if lines:
+        for i in range(
+            n_sqrt * lc.kernel_size[0],
+            n_sqrt * lc.conv_size[0] * lc.kernel_size[0],
+            n_sqrt * lc.kernel_size[0],
+        ):
+            ax.axhline(i - 0.5, color=color, linestyle="--")
 
-            for i in range(
-                n_sqrt * kernel_size[1],
-                n_sqrt * conv_size[1] * kernel_size[1],
-                n_sqrt * kernel_size[1],
-            ):
-                ax.axvline(i - 0.5, color=color, linestyle="--")
+        for i in range(
+            n_sqrt * lc.kernel_size[1],
+            n_sqrt * lc.conv_size[1] * lc.kernel_size[1],
+            n_sqrt * lc.kernel_size[1],
+        ):
+            ax.axvline(i - 0.5, color=color, linestyle="--")
 
-        ax.set_xticks(())
-        ax.set_yticks(())
-        ax.set_aspect("auto")
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_aspect("auto")
 
-        plt.colorbar(im, cax=cax)
-        fig.tight_layout()
-    else:
-        im.set_data(reshaped)
+    plt.colorbar(im, cax=cax)
+    fig.tight_layout()
 
     return im
    
-def plot_locally_connected_output_weights(
-    weights: torch.Tensor,
-    weights_fc: torch.Tensor,
+   
+def plot_FC_response_map(lc: object,
+    fc: object,
     ind_neuron_in_group: int,
     label: int,
-    n_per_class : int,
-    n_filters: int,
-    in_chans: int,
-    slice_to_plot: int,
-    input_size: int,
-    kernel_size: Union[int, Tuple[int, int]],
-    conv_size: Union[int, Tuple[int, int]],
-    wmin: float = 0.0,
-    wmax: float = 1.0,
-    im: Optional[AxesImage] = None,
+    n_per_class: int,
+    input_channel: int = 0,
+    scale_factor: float = 1.0,
     lines: bool = True,
     figsize: Tuple[int, int] = (5, 5),
     cmap: str = "hot_r",
     color: str='r'
-) -> AxesImage:
+    ) -> AxesImage:
+    # language=rst
     """
-    Plot the weights multiplication of LC and output weights, 
-    for a given neuron in one of the output neuron groups in BioLCNet
-    :param weights: Weight matrix of LocalConnection2D object.
-    :param weights_fc: Weight matrix of Connection object.
-    :param label: The neuron group class label
-    :param n_per_class: The number of neurons per class
-    :param n_filters: No. of convolution kernels in use.
-    :param in_channels: No. of input channels
-    :param slice_to_plot: The 2D slice to plot
-    :param inp_size: The square input size
-    :param kernel_size: Side length(s) of 2D convolution kernels.
-    :param conv_size: Side length(s) of 2D convolution population.
-    :param wmin: Minimum allowed weight value.
-    :param wmax: Maximum allowed weight value.
-    :param im: Used for re-drawing the weights plot.
-    :param lines: Whether or not to draw horizontal and vertical lines separating input
-        regions.
+    Plot a connection weight matrix of a :code:`Connection` with `locally connected
+    structure <http://yann.lecun.com/exdb/publis/pdf/gregor-nips-11.pdf>_.
+    :param lc: LC connection object of LCNet
+    :param fc: FC connection object of LCNet
+    :param input_channel: indicates weights which connected to this channel of input
+    :param scale_factor: determines intensity of activation map  
+    :param lines: Whether or not to draw horizontal and vertical lines separating input regions.
     :param figsize: Horizontal, vertical figure size in inches.
     :param cmap: Matplotlib colormap.
     :return: Used for re-drawing the weights plot.
     """
-    n_sqrt = int(np.ceil(np.sqrt(n_filters)))
 
-    sel_slice = weights.view(in_chans, n_filters, conv_size, conv_size, kernel_size, kernel_size).cpu()
-    sel_slice = sel_slice[slice_to_plot, ...]
+    n_sqrt = int(np.ceil(np.sqrt(lc.out_channels)))
 
-    reshaped = reshape_locally_connected_weights(
-        sel_slice, n_filters, kernel_size, conv_size, input_size
-    )
-
+    sel_slice = lc.w.view(lc.in_channels, lc.out_channels, lc.conv_size[0], lc.conv_size[1], lc.kernel_size[0], lc.kernel_size[1]).cpu()
+    sel_slice = sel_slice[input_channel, ...]
+    input_size = _pair(int(np.sqrt(lc.source.n)))
+    reshaped = reshape_LC_weights(sel_slice, lc.out_channels, lc.kernel_size, lc.conv_size, input_size)
+	
     ind_neuron = label * n_per_class + ind_neuron_in_group
-    w = weights_fc[:,ind_neuron].view(reshaped.shape[0]//kernel_size,reshaped.shape[1]//kernel_size)
-    w = w.clip(wmin,wmax).repeat_interleave(kernel_size, dim=0).repeat_interleave(kernel_size, dim=1).cpu()
+    w = fc.w[:,ind_neuron].view(reshaped.shape[0]//lc.kernel_size[0],reshaped.shape[1]//lc.kernel_size[1])
+    w = w.clip(lc.wmin,lc.wmax).repeat_interleave(lc.kernel_size[0], dim=0).repeat_interleave(lc.kernel_size[1], dim=1).cpu()
 
-    if not im:
-        fig, ax = plt.subplots(figsize=figsize)
+    fig, ax = plt.subplots(figsize=figsize)
 
-        im = ax.imshow(reshaped.cpu()*w, cmap=cmap, vmin=wmin, vmax=wmax)
-        div = make_axes_locatable(ax)
-        cax = div.append_axes("right", size="5%", pad=0.05)
-        kernel_size = _pair(kernel_size)
-        conv_size = _pair(conv_size)
-        
-        if lines:
-            for i in range(
-                n_sqrt * kernel_size[0],
-                n_sqrt * conv_size[0] * kernel_size[0],
-                n_sqrt * kernel_size[0],
-            ):
-                ax.axhline(i - 0.5, color=color, linestyle="--")
+    im = ax.imshow(reshaped.cpu()*w, cmap=cmap, vmin=lc.wmin, vmax=lc.wmax)
+    div = make_axes_locatable(ax)
+    cax = div.append_axes("right", size="5%", pad=0.05)
 
-            for i in range(
-                n_sqrt * kernel_size[1],
-                n_sqrt * conv_size[1] * kernel_size[1],
-                n_sqrt * kernel_size[1],
-            ):
-                ax.axvline(i - 0.5, color=color, linestyle="--")
+    if lines:
+        for i in range(
+            n_sqrt * lc.kernel_size[0],
+            n_sqrt * lc.conv_size[0] * lc.kernel_size[0],
+            n_sqrt * lc.kernel_size[0],
+        ):
+            ax.axhline(i - 0.5, color=color, linestyle="--")
 
-        ax.set_xticks(())
-        ax.set_yticks(())
-        ax.set_aspect("auto")
+        for i in range(
+            n_sqrt * lc.kernel_size[1],
+            n_sqrt * lc.conv_size[1] * lc.kernel_size[1],
+            n_sqrt * lc.kernel_size[1],
+        ):
+            ax.axvline(i - 0.5, color=color, linestyle="--")
 
-        plt.colorbar(im, cax=cax)
-        fig.tight_layout()
-    else:
-        im.set_data(reshaped)
+    ax.set_xticks(())
+    ax.set_yticks(())
+    ax.set_aspect("auto")
+
+    plt.colorbar(im, cax=cax)
+    fig.tight_layout()
 
     return im
