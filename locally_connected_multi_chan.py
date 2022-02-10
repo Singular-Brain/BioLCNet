b0bd2bea068b933b234daf2b9f7eae076f00bcb0
Mahbod <mahbodnouri@gmail.com>
2022-02-10 09:36:30 +0000

    minor change

--- locally_connected_multi_chan.py
+++ locally_connected_multi_chan.py
@@ -194,12 +194,12 @@ class DepthWiseLocalConnection2D(LocalConnection2D):
             self.out_channels % (self.in_channels - self.kernel_depth + 1) ,
             f"out_channels (={self.out_channels}) must be a divisible by in_channels - kernel_depth + 1 (={self.in_channels - self.kernel_depth + 1})"
         )
-        n_same_depth_filters = self.out_channels // (self.in_channels - self.kernel_depth + 1) 
+        self.n_same_depth_filters = self.out_channels // (self.in_channels - self.kernel_depth + 1) 
         self.mask = torch.zeros_like(self.w)
         for depth in range(self.in_channels - self.kernel_depth):
             self.mask[
                 depth:depth+self.kernel_depth, 
-                n_same_depth_filters*depth*self.conv_prod:n_same_depth_filters*(depth+1)*self.conv_prod,
+                self.n_same_depth_filters*depth*self.conv_prod:self.n_same_depth_filters*(depth+1)*self.conv_prod,
                 :] = 1
     
     
